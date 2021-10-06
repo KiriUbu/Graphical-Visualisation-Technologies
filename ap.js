e23459a73736c5b239501fa9507060f80122d5f5
Kyrill Klaus <kyrill.klaus@gmx.de>
2021-10-06 18:52:58 +0200

    Ap js eingefügt und webGl wird geladen. Mit Fehler meldungen

---
+++ ap.js
@@ -0,0 +1,15 @@
+var InitDemo = function (){
+    console.log("Alles funktioniert");
+
+    var canvas = document.getElementById("WebGlCanvas")
+    var gl = canvas.getContext('webgl');
+
+    if(!gl){
+        console.log("WebGL not suporet. Dhaer wird alles experimantal ")
+        gl = canvas.getContext('experimantal-webgl');
+    }
+
+    if(!gl){
+        alert("dein Browser unterstützt kein WebGl")
+    }
+};
