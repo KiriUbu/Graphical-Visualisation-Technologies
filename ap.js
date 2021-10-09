23c52fc94cf97f310d92431b45bb09b6aa27ab42
Kyrill Klaus <kyrill.klaus@gmx.de>
2021-10-09 15:47:30 +0200

    ready to take off

--- ap.js
+++ ap.js
@@ -126,12 +126,12 @@ var InitDemo = function (){
     );
 
     gl.vertexAttribPointer(
-        positionAttribLocation, // Atribute location
+        colorAttribLocation, // Atribute location
         3, // Number of elements per attribute
         gl.FLOAT,
         gl.FALSE,
         5* Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-        0 // Offset from the beginning of a single vertex to thi attribute
+        3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to thi attribute
 
     );
     gl.enableVertexAttribArray(positionAttribLocation);
